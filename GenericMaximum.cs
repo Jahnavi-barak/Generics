dbc31739b079a40d918c317431ac794727177da6
jahnavi <kosalajahnavi656@gmail.com>
2021-10-24 17:03:55 +0530

    genericsMaxNumbers

---
+++ GenericMaximum.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMaxTwoNum
+{
+    public class GenericMaximum<T> where T : IComparable
+    {
+        public T[] Value;
+
+        /// <summary>
+        /// this method is created for intialise the constructor of instance variable
+        /// </summary>
+        /// <param name="input_val1"></param>
+        /// <param name="input_val2"></param>
+        /// <param name="input_val3"></param>
+        public GenericMaximum(T[] value)
+        {
+            this.Value = value;
+        }
+        /// <summary>
+        /// This method is created for finding max for given type
+        /// params is used for optional
+        /// </summary>
+        /// <param name="val1"></param>
+        /// <param name="val2"></param>
+        /// <param name="val3"></param>
+        /// <returns></returns>
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = (values);
+            return sorted_values[1];
+        }
+
+        /// <summary>
+        /// This method is created calling maxvalue method
+        /// </summary>
+        /// <returns>max</returns>
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.Value);
+            return max;
+        }
+
+        /// <summary>
+        /// This  method is printing maximum generic value
+        /// </summary>
+        /// <param name="="values"></param>
+        public void printMaxValue()
+        {
+            var max = MaxValue(this.Value);
+            Console.WriteLine("Maximum Value is " + max);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 112, 344, 432, 555, 678 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+            generic.printMaxValue();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMaxTwoNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Num");
+
+            //int output = MaximumIntegerNumber(10,20,30);
+            //Console.WriteLine(output);
+
+            //double doubleOutput = MaximumNumberCheck.MaximumFloatNumber(10.23,20.34,33.32);
+            //Console.WriteLine(doubleOutput);
+
+            //string stringOutput = MaximumNumberCheck.MaximumStringNumber(10.23,20.34,33.32);
+            //Console.WriteLine(stringOutput);
+
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.printMaxValue();
+
+            double[] doubleArray = { 11.2, 34.4, 4.32, 55.3, 6, 78 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.printMaxValue();
+
+            string[] stringArray = { "111","222","323","425", "543" };
+            GenericMaximum<string> genericstring = new GenericMaximum<string>(stringArray);
+            genericstring.printMaxValue();
+        }
+    }
+}
